9d50ecbf3dc71f05cc34d13303935823322e1732
Naomi Johnson <naomi_johnson123@yahoo.com>
2025-02-05 17:34:01 -0500

    Ajay changed angles

--- src/mie443_contest_1/src/state.cpp
+++ src/mie443_contest_1/src/state.cpp
@@ -86,11 +86,11 @@ void robotState::update() {
         switch (stateRef.bumperHit) {
 
         case BumperHit::LEFT:
-          allSorted = doTurn(45, stateRef.yaw, true);
+          allSorted = doTurn(90, stateRef.yaw, true);
           break;
 
         case BumperHit::RIGHT:
-          allSorted = doTurn(-45, stateRef.yaw, true);
+          allSorted = doTurn(-90, stateRef.yaw, true);
           break;
 
         case BumperHit::CENTER:
@@ -107,11 +107,11 @@ void robotState::update() {
         switch (stateRef.bumperHit) {
 
         case BumperHit::LEFT:
-          allSorted = doTurn(-135, stateRef.yaw, true);
+          allSorted = doTurn(-90, stateRef.yaw, true);
           break;
 
         case BumperHit::RIGHT:
-          allSorted = doTurn(135, stateRef.yaw, true);
+          allSorted = doTurn(90, stateRef.yaw, true);
           break;
 
         case BumperHit::CENTER:
