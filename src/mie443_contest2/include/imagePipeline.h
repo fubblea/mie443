0ebf220a1cb352a30bd6515bd91b96c23aafead7
Ajay Anand <ajay.arthur@gmail.com>
2025-02-27 06:57:10 -0800

    Setup some stuff

--- src/mie443_contest2/include/imagePipeline.h
+++ src/mie443_contest2/include/imagePipeline.h
@@ -1,19 +1,20 @@
 #pragma once
 
-#include <image_transport/image_transport.h>
-#include <std_msgs/String.h>
-#include <opencv2/core.hpp>
-#include <cv.h>
-#include <cv_bridge/cv_bridge.h>
 #include <boxes.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <opencv2/core/core.hpp>
+#include <std_msgs/String.h>
 
 class ImagePipeline {
-    private:
-        cv::Mat img;
-        bool isValid;
-        image_transport::Subscriber sub;
-    public:
-        ImagePipeline(ros::NodeHandle& n);
-        void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-        int getTemplateID(Boxes& boxes);
+private:
+  cv::Mat img;
+  bool isValid;
+  image_transport::Subscriber sub;
+
+public:
+  ImagePipeline(ros::NodeHandle &n);
+  void imageCallback(const sensor_msgs::ImageConstPtr &msg);
+  int getTemplateID(Boxes &boxes);
 };
