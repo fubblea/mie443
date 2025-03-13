f93b0cbdd8d86905f94a4401cf9bcb8f43e09dd7
Ajay Anand <ajay.arthur@gmail.com>
2025-03-13 12:34:22 -0700

    Created testbench module

---
+++ src/contest2_testbench/include/contest2_testbench/testbench.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <contest2/contest2.h>
+#include <contest2/imagePipeline.h>
---
+++ src/contest2_testbench/src/testbench.cpp
@@ -0,0 +1,3 @@
+#include <contest2_testbench/testbench.h>
+
+int main(int argc, char **argv) { return 0; }
