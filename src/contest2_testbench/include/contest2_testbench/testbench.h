0350f8072641cf053e28e7aa13cfc1f336c66669
Ajay Anand <ajay.arthur@gmail.com>
2025-03-13 12:55:07 -0700

    Added testrunner node

--- src/contest2_testbench/include/contest2_testbench/testbench.h
+++ src/contest2_testbench/include/contest2_testbench/testbench.h
@@ -1,4 +1,10 @@
 #pragma once
 
+// INCLUDE FILES
 #include <contest2/contest2.h>
 #include <contest2/imagePipeline.h>
+
+// CONSTANTS
+
+const bool SHOW_VIEW = false;
+const bool SHOW_MATCHES = false;
---
+++ src/contest2_testbench/src/testrunner.cpp
@@ -0,0 +1,3 @@
+#include <contest2_testbench/testrunner.h>
+
+int main(int argc, char **argv) { return 0; }
