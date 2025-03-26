089802e8ff6d93fc924d5bb8742928ae299de12b
satsab02 <sathvikbasrica@gmail.com>
2025-03-26 13:00:07 -0700

    Added debug messages

--- src/mie443_contest3/src/state_machine.cpp
+++ src/mie443_contest3/src/state_machine.cpp
@@ -1,11 +1,14 @@
 #include "ros/console.h"
 #include "ros/init.h"
+#include <contest3/constest3.h>
 #include <contest3/state.h>
 
 void RobotState::updateState(float secondsElapsed, bool contestMode) {
   switch (this->currState) {
   case State::START: {
+    ROS_INFO("IT BEGINS");
     this->sc.playWave(SOUND_PATHS + "sound.wav");
+    ROS_INFO("Did the sound play");
     ros::Duration(0.5).sleep();
 
     setState(State::FOLLOWING);
@@ -14,8 +17,10 @@ void RobotState::updateState(float secondsElapsed, bool contestMode) {
 
   case State::FOLLOWING: {
     if (this->checkBumper() == BumperHit::NOTHING) {
+      ROS_INFO("Bumper is clean, following you!")
       setVelCmd(this->follow_cmd);
     } else {
+      ROS_INFO("Seems like a bumper is hit. Switching State");
       setState(State::IM_HIT);
     }
 
@@ -23,9 +28,12 @@ void RobotState::updateState(float secondsElapsed, bool contestMode) {
   }
 
   case State::IM_HIT: {
-    ROS_INFO("Im hit!");
-    setVelCmd(0, 0);
-    sc.playWave(std::string("src/mie443_contest3/sounds") + "PAIN.wav");
+
+    if (this->checkBumper() != BumperHit::NOTHING) {
+      ROS_INFO("Im hit!");
+      setVelCmd(0, 0);
+      sc.playWave(SOUND_PATHS + "PAIN.wav");
+    }
 
     break;
   }
