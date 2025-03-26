e5486ed91683ffe8706a97238df59e40e49dfcdf
Naomi Johnson <naomi_johnson123@yahoo.com>
2025-03-26 18:25:45 -0400

    Something

--- src/mie443_contest3/src/state_machine.cpp
+++ src/mie443_contest3/src/state_machine.cpp
@@ -58,8 +58,10 @@ void RobotState::updateState(float secondsElapsed, bool contestMode) {
 
       ROS_INFO("Following backward");
       if (findFollowState(this->follow_cmd) == State::FOLLOW_BACK) {
-        std::async(std::launch::async,
-                   [&]() -> void { sc.playWave(SOUND_PATHS + "Disgust.wav"); });
+        std::async(std::launch::async, [&]() -> void {
+          sc.playWave(SOUND_PATHS + "Disgust.wav");
+          ros::Duration(3.0).sleep();
+        });
         setVelCmd(this->follow_cmd);
       } else {
         setState(findFollowState(this->follow_cmd));
